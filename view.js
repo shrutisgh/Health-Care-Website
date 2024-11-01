ee54d12a2110547296e87a4adbd816799a2ef12c
Shruti Singh <148178631+shrutisgh@users.noreply.github.com>
2024-11-01 17:16:34 +0530

    Add files via upload

--- view.js
+++ view.js
@@ -5,56 +5,32 @@
 import View from '../../base/view';
 import template from './template';
 
-export default class QuestionView extends View {
+export default class SuggestView extends View {
   constructor(el, context) {
-    const handleNextQuestion = (e) => {
+    context.data = context.patient.toSuggest();
+
+    const handleSymptomsChange = (e) => {
       const group = {};
-      if (this.context.question.type !== 'single') {
-        this.el.querySelectorAll('.custom-control-input').forEach((item) => {
-          group[item.id] = {reported: item.checked};
-        });
-        this.context.patient.addSymptomsGroup(group);
-        this.destroy();
-        this.render();
-      } else {
-        const val = {
-          false: false,
-          true: true,
-          'unknown:': undefined
-        };
-        this.context.patient.addSymptomsGroup({
-          [this.context.question.items[0].id]: {reported: val[e.target.dataset.value]}
-        });
-        this.destroy();
-        this.render();
-      }
+      this.el.querySelectorAll('.input-symptom').forEach((item) => {
+        // we do not mark any symptoms that comes from suggest as absent
+        if (item.checked) {
+          group[item.id] = {reported: true, source: 'suggest'};
+        } else {
+          // completely remove this symptom
+          this.context.patient.removeSymptom(item.id);
+        }
+      });
+
+      this.context.patient.addSymptomsGroup(group);
     };
 
     const binds = {
-      '.next-question': {
-        type: 'click',
-        listener: handleNextQuestion
+      '.input-symptom': {
+        type: 'change',
+        listener: handleSymptomsChange
       }
     };
 
     super(el, template, context, binds);
-  }
-
-  render() {
-    this.el.innerHTML = '<p><i class="fa fa-circle-o-notch fa-spin fa-fw"></i> I am thinking...</p>';
-    this.context.api.diagnosis(this.context.patient.toDiagnosis()).then((data) => {
-      this.context.question = data.question;
-
-      // check stop condition
-      if (data.should_stop === true) {
-        this.destroy();
-        document.getElementById('next-step').removeAttribute('disabled');
-        document.getElementById('next-step').click();
-      } else {
-        super.render();
-      }
-
-      return data;
-    });
   }
 }
