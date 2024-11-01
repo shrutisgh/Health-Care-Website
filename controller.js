efc2f3b5667c645cc850e3c650cfef7dd932ded6
Shruti Singh <148178631+shrutisgh@users.noreply.github.com>
2024-11-01 17:57:16 +0530

    Add files via upload

--- controller.js
+++ controller.js
@@ -1,28 +1,37 @@
 /**
- * Created by Tomasz Gabrysiak @ Infermedica on 06/02/2017.
+ * Created by Tomasz Gabrysiak @ Infermedica on 14/02/2017.
  */
 
-export default class Controller {
+import Controller from './base/controller';
+
+import WelcomeView from './components/welcome/view';
+import BasicView from './components/basic/view';
+import SuggestView from './components/suggest/view';
+import NLPView from './components/nlp/view';
+import GeoRisksView from './components/geo-risks/view';
+import CommonRisksView from './components/common-risks/view';
+import QuestionView from './components/question/view';
+import SummaryView from './components/summary/view';
+
+export default class DemoController extends Controller {
   constructor(el) {
-    this.el = el;
+    super(el);
+    this.viewMapper = {
+      welcome: WelcomeView,
+      basic: BasicView,
+      suggest: SuggestView,
+      nlp: NLPView,
+      'geo-risks': GeoRisksView,
+      'common-risks': CommonRisksView,
+      question: QuestionView,
+      summary: SummaryView
+    };
   }
 
-  render() {
-    this.view.render();
-  }
-
-  setView(name, context) {
-    this.beforeSetView(name);
+  beforeSetView(name) {
     const ViewClass = this.viewMapper[name];
-    this.view = new ViewClass(this.el, context);
-    this.render();
-  }
-
-  destroyView() {
-    this.view.destroy();
-  }
-
-  beforeSetView(name) {  // eslint-disable-line
-    // intentionally left blank
+    if ([QuestionView, NLPView].includes(ViewClass)) {
+      document.getElementById('next-step').setAttribute('disabled', 'true');
+    }
   }
 }
