45ec588a5b983538f1e338f6997048687b7f3715
Shruti Singh <148178631+shrutisgh@users.noreply.github.com>
2024-11-01 17:12:45 +0530

    Add files via upload

--- template.js
+++ template.js
@@ -2,21 +2,42 @@
  * Created by Tomasz Gabrysiak @ Infermedica on 08/02/2017.
  */
 
-import html, {riskHtmlMapper} from '../../templates/helpers';
+import html from '../../templates/helpers';
 
 const template = (context) => {
-  return context.api.getRiskFactors(context.patient.age).then((risks) => {
-    return html`
-      <h5 class="card-title">Please select where you live or have recently traveled to.</h5>
+  return new Promise((resolve) => {
+    resolve(html`
+      <h5 class="card-title">Tell us how you feel.</h5>
       <div class="card-text">
         <form>
-          ${riskHtmlMapper(risks, context.locationRiskFactors)}
+          <div class="form-group">
+            <label for="input-feel">
+              We will try to recognize your symptoms using Natural Language Processing algorithms.
+            </label>
+            <textarea placeholder="e.g. I got headache" class="form-control" id="input-feel" rows="4"></textarea>
+          </div>
         </form>
+        <p>Identified observations:</p>
+        <ul class="list-unstyled" id="observations">
+        </ul>
         <p class="text-muted small">
-          <i class="fa fa-info-circle"></i> Above you see risk factors related to geographical location.
+          <i class="fa fa-exclamation-circle"></i>
+          Please note that you can go <span class="badge badge-primary">Next</span> only if there are 
+          <span class="text-success">present <i class="fa fa-plus-circle"></i></span>
+          identified observations.
+        </p>
+        <p class="text-muted small">
+          <i class="fa fa-info-circle"></i> This screen uses our NLP engine to find symptoms in a written text.
+          Evidence found in text will be marked as initial which is important to our engine. 
+          Please read more about initial evidence 
+          <a target="_blank" href="https://developer.infermedica.com/docs/diagnosis#gathering-initial-evidence">
+            here
+          </a>.
+          All of the identified symptoms will be added to your interview after clicking
+          <span class="badge badge-primary">Next</span>.
         </p>
       </div>
-    `;
+    `);
   });
 };
 
