f1da65e93e5843f418b8104d43fcffd9f4e61215
Shruti Singh <148178631+shrutisgh@users.noreply.github.com>
2024-11-01 17:11:22 +0530

    Add files via upload

--- template.js
+++ template.js
@@ -7,21 +7,16 @@ import html, {riskHtmlMapper} from '../../templates/helpers';
 const template = (context) => {
   return context.api.getRiskFactors(context.patient.age).then((risks) => {
     return html`
-        <h5 class="card-title">Please check all that apply to you.</h5>
-        <div class="card-text">
-          <form>
-            ${riskHtmlMapper(risks, context.commonRiskFactors)}
-          </form>
-          <p class="text-muted small">
-            <i class="fa fa-info-circle"></i> Above you see the most common risk factors. 
-            Although /diagnosis may return questions about risk factors, 
-            when implementing a symptom checker we recommend asking the patient about common risk factors 
-            before the actual interview begins. This helps to steer the interview in the right direction 
-            and to reduce its length. Please read more about risk factors 
-            <a target="_blank" href="https://developer.infermedica.com/docs/diagnosis#common-risk-factors">here</a>.
-          </p>
-        </div>
-      `;
+      <h5 class="card-title">Please select where you live or have recently traveled to.</h5>
+      <div class="card-text">
+        <form>
+          ${riskHtmlMapper(risks, context.locationRiskFactors)}
+        </form>
+        <p class="text-muted small">
+          <i class="fa fa-info-circle"></i> Above you see risk factors related to geographical location.
+        </p>
+      </div>
+    `;
   });
 };
 
