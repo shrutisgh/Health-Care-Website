551831697e14c7dd05f62f41b6ad460596471a02
Shruti Singh <148178631+shrutisgh@users.noreply.github.com>
2024-11-01 17:14:12 +0530

    Add files via upload

--- template.js
+++ template.js
@@ -4,38 +4,63 @@
 
 import html from '../../templates/helpers';
 
+const answersGroupSingle = (items) => {
+  return html`
+    <form>
+      <fieldset class="form-group">
+  ${items.map((i) => {
+    return html`              
+        <div class="custom-control custom-radio">
+          <input type="radio" id="${i.id}" name="radio" class="custom-control-input">
+          <label class="custom-control-label" for="${i.id}">${i.name}</label>
+        </div>
+    `;
+  })}
+      </fieldset>
+    </form>
+  `;
+};
+
+const answersGroupMultiple = (items) => {
+  return html`
+    <form>
+      <fieldset class="form-group">
+  ${items.map((i) => {
+    return html`
+        <div class="custom-control custom-checkbox">
+          <input id="${i.id}" type="checkbox" class="custom-control-input">
+          <label for="${i.id}" class="custom-control-label custom-checkbox mb-2 mr-sm-2 mb-sm-0">${i.name}</label>
+        </div>     
+    `;
+  })}
+      </fieldset>
+    </form>
+  `;
+};
+
+const answersSingle = () => {
+  return html`
+    <div>
+      <button type="button" data-value="true" class="next-question btn btn-success">Yes</button>
+      <button type="button" data-value="false" class="next-question btn btn-danger">No</button>
+      <button type="button" data-value="unknown" class="next-question btn btn-info">Skip question</button>
+    </div>
+  `;
+};
+
 const template = (context) => {
   return new Promise((resolve) => {
+    const mapper = {
+      group_single: answersGroupSingle,
+      group_multiple: answersGroupMultiple,
+      single: answersSingle
+    };
     resolve(html`
-      <h5 class="card-title">Tell us how you feel.</h5>
+      <h5 class="card-title">${context.question.text}</h5>
       <div class="card-text">
-        <form>
-          <div class="form-group">
-            <label for="input-feel">
-              We will try to recognize your symptoms using Natural Language Processing algorithms.
-            </label>
-            <textarea placeholder="e.g. I got headache" class="form-control" id="input-feel" rows="4"></textarea>
-          </div>
-        </form>
-        <p>Identified observations:</p>
-        <ul class="list-unstyled" id="observations">
-        </ul>
-        <p class="text-muted small">
-          <i class="fa fa-exclamation-circle"></i>
-          Please note that you can go <span class="badge badge-primary">Next</span> only if there are 
-          <span class="text-success">present <i class="fa fa-plus-circle"></i></span>
-          identified observations.
-        </p>
-        <p class="text-muted small">
-          <i class="fa fa-info-circle"></i> This screen uses our NLP engine to find symptoms in a written text.
-          Evidence found in text will be marked as initial which is important to our engine. 
-          Please read more about initial evidence 
-          <a target="_blank" href="https://developer.infermedica.com/docs/diagnosis#gathering-initial-evidence">
-            here
-          </a>.
-          All of the identified symptoms will be added to your interview after clicking
-          <span class="badge badge-primary">Next</span>.
-        </p>
+        ${mapper[context.question.type](context.question.items)}
+        ${context.question.type !== 'single'
+    ? '<button class="next-question btn btn-primary">Next question</button>' : ''}
       </div>
     `);
   });
