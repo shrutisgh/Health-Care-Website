27f972839897f024092d3109fc30a21df7b5a778
Shruti Singh <148178631+shrutisgh@users.noreply.github.com>
2024-11-01 17:18:25 +0530

    Add files via upload

--- template.js
+++ template.js
@@ -2,43 +2,42 @@
  * Created by Tomasz Gabrysiak @ Infermedica on 08/02/2017.
  */
 
-import html from '../../templates/helpers';
-
-const conditionsHtmlMapper = (conditions) => {
-  return conditions.map((condition) => `
-    <div class="summary-item row">
-      <div class="col-8">
-        ${condition.name}
-        ${condition.probability >= 0.2
-    ? `<i class="fa fa-fw fa-eye"></i><a href data-id="${condition.id}" class="explain">explain</a>` : ''}
-      </div>
-      <div class="col-4">
-        <div class="progress">
-          <div class="progress-bar bg-info" role="progressbar" 
-              style="width: ${Math.floor(condition.probability * 100)}%">
-            ${Math.floor(condition.probability * 100)}%
-          </div>
-        </div>
-      </div>
-      <div class="explanation col-12"></div>
-    </div>          
-  `);
-};
-
 const template = (context) => {
-  return context.api.diagnosis(context.patient.toDiagnosis()).then((data) => {
-    return html`
-      <h5 class="card-title">Summary</h5>
+  return new Promise((resolve) => {
+    resolve(`
+      <h5 class="card-title">Welcome to the Symptom Checker Demo.</h5>
       <div class="card-text">
-        <p>Based on the interview, you could suffer from:</p>
-        ${conditionsHtmlMapper(data.conditions)}
-        <div class="alert alert-warning" role="alert">
-          <i class="fa fa-info-circle"></i>
-          Please note that the list below may not be complete and is provided solely for informational purposes 
-          and is not a qualified medical opinion.
-        </div>
+        <p>
+          We created this example to help you work with our API, 
+          and show you how to implement a simple symptom checker &mdash; 
+          an application that mimics doctor's interview and gives you a preliminary diagnosis basing on 
+          our mathematical model.
+        </p>
+        <p>
+          Visit our 
+          <i class="fa fa-github"></i> 
+          <a href="https://github.com/infermedica/js-symptom-checker-example">GitHub page</a> 
+          to grab the source code of this example as well as a guide on how to start working with it.
+        </p>
+        <p>
+          The most recent API documentation is always available on our 
+          <a href="https://developer.infermedica.com/docs/api">developer portal</a>.
+        </p>
+        <p>
+          Please click 
+          <span class="badge badge-primary">Next</span> 
+          to move to the first question.
+        </p>
+        <p class="text-muted small">
+          <i class="fa fa-info-circle"></i> 
+          This is the welcome screen. In a real life scenario you will probably want to display T&C here. 
+          In the background, the new 
+          <a target="_blank" href="https://developer.infermedica.com/docs/diagnosis#helping-us-improve-the-engine">
+            Interview-Id
+          </a>
+          was created.</p>
       </div>
-    `;
+    `);
   });
 };
 
