524e87bda5f8807d3c939318f838de200e3c039f
Shruti Singh <148178631+shrutisgh@users.noreply.github.com>
2024-11-01 17:16:13 +0530

    Add files via upload

--- template.js
+++ template.js
@@ -2,67 +2,41 @@
  * Created by Tomasz Gabrysiak @ Infermedica on 08/02/2017.
  */
 
+import _ from 'lodash';
 import html from '../../templates/helpers';
 
-const answersGroupSingle = (items) => {
-  return html`
-    <form>
-      <fieldset class="form-group">
-  ${items.map((i) => {
-    return html`              
-        <div class="custom-control custom-radio">
-          <input type="radio" id="${i.id}" name="radio" class="custom-control-input">
-          <label class="custom-control-label" for="${i.id}">${i.name}</label>
-        </div>
-    `;
-  })}
-      </fieldset>
-    </form>
-  `;
-};
-
-const answersGroupMultiple = (items) => {
-  return html`
-    <form>
-      <fieldset class="form-group">
-  ${items.map((i) => {
+const symptomHtmlMapper = (suggestedSymptoms) => {
+  return _.take(suggestedSymptoms, 5).map((symptom) => {
     return html`
-        <div class="custom-control custom-checkbox">
-          <input id="${i.id}" type="checkbox" class="custom-control-input">
-          <label for="${i.id}" class="custom-control-label custom-checkbox mb-2 mr-sm-2 mb-sm-0">${i.name}</label>
-        </div>     
+      <div class="custom-control custom-checkbox">
+        <input id="${symptom.id}" type="checkbox" class="input-symptom custom-control-input">
+        <label for="${symptom.id}" class="custom-control-label custom-checkbox mb-2 mr-sm-2 mb-sm-0">
+          ${symptom.name}
+        </label>
+      </div>
     `;
-  })}
-      </fieldset>
-    </form>
-  `;
-};
-
-const answersSingle = () => {
-  return html`
-    <div>
-      <button type="button" data-value="true" class="next-question btn btn-success">Yes</button>
-      <button type="button" data-value="false" class="next-question btn btn-danger">No</button>
-      <button type="button" data-value="unknown" class="next-question btn btn-info">Skip question</button>
-    </div>
-  `;
+  });
 };
 
 const template = (context) => {
-  return new Promise((resolve) => {
-    const mapper = {
-      group_single: answersGroupSingle,
-      group_multiple: answersGroupMultiple,
-      single: answersSingle
-    };
-    resolve(html`
-      <h5 class="card-title">${context.question.text}</h5>
+  return context.api.getSuggestedSymptoms(context.data).then((suggestedSymptoms) => {
+    if (!suggestedSymptoms.length) {
+      document.getElementById('next-step').click();
+      return '<p><i class="fa fa-circle-o-notch fa-spin fa-fw"></i> I am thinking...</p>';
+    }
+    return html`
+      <h5 class="card-title">Do you have any of the following symptoms?</h5>
       <div class="card-text">
-        ${mapper[context.question.type](context.question.items)}
-        ${context.question.type !== 'single'
-    ? '<button class="next-question btn btn-primary">Next question</button>' : ''}
+        <form>
+          ${symptomHtmlMapper(suggestedSymptoms)}
+        </form>
+        <p class="text-muted small">
+          <i class="fa fa-info-circle"></i>
+          This is a list of symptoms suggested by our AI,
+          based on the information gathered so far during the interview.
+        </p>
       </div>
-    `);
+    `;
   });
 };
 
